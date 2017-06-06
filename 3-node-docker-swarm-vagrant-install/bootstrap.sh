e8b4352eaf2dc0dbec2af1abb31dc11eaa070b04
Brent Salisbury <brent.salisbury@docker.com>
2017-06-06 18:29:33 -0400

    Update bootstrap.sh

--- 3-node-docker-swarm-vagrant-install/bootstrap.sh
+++ 3-node-docker-swarm-vagrant-install/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y linux-generic-lts-wily netcat iproute2 apt-transport-https ca-certificates curl
+sudo apt-get install -y linux-generic-lts-wily netcat iproute2 apt-transport-https ca-certificates curl software-properties-common python-software-properties
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download-stage.docker.com/linux/ubuntu $(lsb_release -cs) test"
 sudo apt-get update
