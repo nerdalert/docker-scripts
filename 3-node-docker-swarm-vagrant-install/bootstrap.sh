c15d7ffe0743dbc157741c97cfbaca6887fea94e
Brent Salisbury <brent@socketplane.io>
2017-06-06 18:09:19 -0400

    3-node vagrant swarm

---
+++ 3-node-docker-swarm-vagrant-install/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y linux-generic-lts-wily netcat iproute2 apt-transport-https ca-certificates curl
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download-stage.docker.com/linux/ubuntu $(lsb_release -cs) test"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo usermod -aG docker vagrant
+sudo reboot
